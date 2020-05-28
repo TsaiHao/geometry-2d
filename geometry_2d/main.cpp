e9fd9f42e3cbbcfcf332fefb3d756503fd62155a
TsaiHao <zaijun521@gmail.com>
2020-05-29 00:42:26 +0800

    first commit

---
+++ geometry_2d/main.cpp
@@ -0,0 +1,16 @@
+#include "shape.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	Circle circle(1, 1, 2);
+	Triangle triangle(0, 0, 5, 4, 5, -3);
+	Rectangle rectangle(2, 0, 6, -10);
+
+	cout << boolalpha;
+	cout << Intersect(&circle, &triangle) << endl
+		<< Intersect(&circle, &rectangle) << endl
+		<< Intersect(&rectangle, &triangle);
+	return 0;
+}
